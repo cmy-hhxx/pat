8b5c649f248312e0969d9c35524e223c5a2e2827
cmy <cmyhhxx@gmail.com>
2022-07-31 22:26:04 +0800

    add 1004 && 1020

---
+++ tree/1004-enumeratingleafs.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+const int N = 110;
+int h[N], e[N], ne[N], idx;
+int n, m;
+int ans[N];
+int max_depth;
+
+void add(int a, int b)
+{
+    e[idx] = b, ne[idx] = h[a], h[a] = idx ++;
+}
+
+void dfs(int u, int depth)
+{
+    if (h[u] == -1) 
+    {
+        ans[depth] ++;
+        max_depth = max(max_depth, depth);
+        return;
+    }
+
+    for (int i = h[u]; ~i; i = ne[i])
+        dfs(e[i], depth + 1);
+}
+
+int main()
+{
+    cin >> n >> m;
+
+    memset(h, -1, sizeof h);
+
+    while (m -- ) 
+    {
+        int id, k;
+        cin >> id >> k;
+        while (k -- )
+        {
+            int son;
+            cin >> son;
+            add(id, son);
+        }
+    }
+
+    dfs(1, 0);
+
+    cout << ans[0];
+    for (int i = 1; i <= max_depth; i ++ ) cout << ' ' << ans[i];
+
+    return 0;
+}
---
+++ tree/1020-treetraveler.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+const int N = 50;
+int q[N];
+int postorder[N], inorder[N];
+unordered_map<int, int> l, r, pos;
+int n;
+
+int build(int il, int ir, int pl, int pr)
+{
+    int root = postorder[pr];
+    int k = pos[root];
+    if (il < k) l[root] = build(il, k - 1, pl, pl + k - 1 - il);
+    if (ir > k) r[root] = build(k + 1, ir, pl + k - il, pr - 1);
+
+    return root;
+}
+
+void bfs(int root)
+{
+    int hh = 0, tt = -1;
+    q[++ tt] = root;
+
+    while (hh <= tt)
+    {
+        int t = q[hh ++];
+        if (l.count(t)) q[++ tt] = l[t];
+        if (r.count(t)) q[++ tt] = r[t];
+    }
+
+    cout << q[0];
+    for (int i = 1; i <= tt; i ++ ) cout << ' ' << q[i];
+
+}
+
+int main()
+{
+    cin >> n;
+
+    for (int i = 0; i < n; i ++ ) cin >> postorder[i];
+    for (int i = 0; i < n; i ++ )
+    {
+        cin >> inorder[i];
+        pos[inorder[i]] = i;
+    }
+
+    int root = build(0, n - 1, 0, n - 1);
+
+    bfs(root);
+
+    return 0;
+}
